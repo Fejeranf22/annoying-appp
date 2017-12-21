36976c133aad5459c12dae8da75ff87a26c028fd
ty fejerang <fejerang.ty@gmail.com>
2017-12-20 16:39:09 -0800

    this is practicing the while loop vanilla javascript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var answer = prompt(" Do you think this app is annoying???");
+  while (answer !== "yes") {
+    var answer =prompt("Well how about now??");
+  }
+
+  alert("HAHAHAHAHAH!!! I call that sucess!!! Ok now back to your scheduled broadcast :}");
