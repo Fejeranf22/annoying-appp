911c10d4a2d61d575d4f5349503ec1308aee9355
ty fejerang <fejerang.ty@gmail.com>
2017-12-20 16:49:28 -0800

    i created an answer log that will take any answer containing yes.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+// var answer = prompt(" Do you think this app is annoying???");
+//   while (answer !== "yes") {
+//     var answer =prompt("Well how about now??");
+//   }
+//
+//   alert("HAHAHAHAHAH!!! I call that sucess!!! Ok now back to your scheduled broadcast :}");
+
+// This is the start of version 2
+// Both versions will work
+// try it out:)
 var answer = prompt(" Do you think this app is annoying???");
-  while (answer !== "yes") {
+  while (answer.indtxof ("yes")== -1) {
     var answer =prompt("Well how about now??");
   }
 
